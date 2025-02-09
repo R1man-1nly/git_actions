7c20fdbc42ce6ab897d4a96310c13e6290a41d64
Roman <r1man.tn@gmail.com>
2025-02-09 15:42:56 +0200

    Add tests

--- actions/action/tests.py
+++ actions/action/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+import http
+
+
+class IndexTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
